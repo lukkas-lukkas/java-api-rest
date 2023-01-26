7527033fd11c10dc7b0d386f469dcc34cb49e673
Lucas Lima <lukkasoliveiralima@gmail.com>
2023-01-26 16:46:55 -0300

    add migration to doctor

---
+++ src/main/resources/db/migration/V1__create-table-doctors.sql
@@ -0,0 +1,15 @@
+create table doctors(
+    id bigint not null auto_increment,
+    name varchar(100) not null,
+    email varchar(100) not null unique,
+    crm varchar(100) not null unique,
+    specialty varchar(100) not null,
+    street varchar(100) not null,
+    neighborhood varchar(100) not null,
+    zipCode varchar(100) not null,
+    city varchar(100) not null,
+    state varchar(100) not null,
+    complement varchar(100),
+
+    primary key(id)
+);
