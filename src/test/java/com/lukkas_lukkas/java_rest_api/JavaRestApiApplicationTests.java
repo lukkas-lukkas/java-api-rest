4261d3cc2854ebfb28470c0bb5e344d626f0ac34
Lucas Lima <lukkasoliveiralima@gmail.com>
2024-05-30 01:05:17 -0300

    first commit

---
+++ src/test/java/com/lukkas_lukkas/java_rest_api/JavaRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukkas_lukkas.java_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
