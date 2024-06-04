63fd306c24b54069bdc685aaaf07c6edd40be2ca
Lucas Lima <lukkasoliveiralima@gmail.com>
2024-06-04 00:43:34 -0300

    add randon uuid to greeting

--- src/main/java/com/lukkas_lukkas/java_rest_api/application/greeting/GreetingHandler.java
+++ src/main/java/com/lukkas_lukkas/java_rest_api/application/greeting/GreetingHandler.java
@@ -2,10 +2,15 @@ package com.lukkas_lukkas.java_rest_api.application.greeting;
 
 import com.lukkas_lukkas.java_rest_api.domain.Greeting;
 
+import java.util.UUID;
+
 public class GreetingHandler {
     private final String template = "Hello, %s!";
 
     public Greeting handle(String name) {
-        return new Greeting(1, String.format(template, name));
+        return new Greeting(
+                UUID.randomUUID(),
+                String.format(template, name)
+        );
     }
 }
