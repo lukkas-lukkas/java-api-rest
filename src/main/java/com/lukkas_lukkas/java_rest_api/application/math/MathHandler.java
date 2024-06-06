ec5846f0c7a18d4c54e9089e4e808dffc72ea1aa
Lucas Lima <lukkasoliveiralima@gmail.com>
2024-06-05 22:25:13 -0300

    apply calculation flow

--- src/main/java/com/lukkas_lukkas/java_rest_api/application/math/MathHandler.java
+++ src/main/java/com/lukkas_lukkas/java_rest_api/application/math/MathHandler.java
@@ -1,10 +1,26 @@
 package com.lukkas_lukkas.java_rest_api.application.math;
 
+import com.lukkas_lukkas.java_rest_api.application.math.calculators.CalculatorFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MathHandler {
-    public Double handle(MathDTO dto) {
-        return 2D;
+    private final CalculatorFactory calculatorFactory;
+
+    public MathHandler(CalculatorFactory calculatorFactory) {
+        this.calculatorFactory = calculatorFactory;
+    }
+
+    public MathResponse handle(MathDTO dto) {
+        var calculator = this.calculatorFactory.factory(dto.operation);
+        Double result = calculator.calculate(
+                dto.numberOne,
+                dto.numberTwo
+        );
+
+        return new MathResponse(
+                dto,
+                result
+        );
     }
 }
