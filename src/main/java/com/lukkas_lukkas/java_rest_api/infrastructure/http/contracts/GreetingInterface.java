e2e047b214cea7fa14d6b4e3eb49748d995d167e
Lucas Lima <lukkasoliveiralima@gmail.com>
2024-07-18 23:21:20 -0300

    create greeting documentation

---
+++ src/main/java/com/lukkas_lukkas/java_rest_api/infrastructure/http/contracts/GreetingInterface.java
@@ -0,0 +1,27 @@
+package com.lukkas_lukkas.java_rest_api.infrastructure.http.contracts;
+
+import com.lukkas_lukkas.java_rest_api.domain.Greeting;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "general")
+public interface GreetingInterface {
+
+    @Operation(
+            summary = "Make a greeting",
+            description = "Receive a greeting with a default value or your name",
+            method = "GET"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Greeting created successfully",
+            content = {@Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = Greeting.class)
+            )}
+    )
+    public Greeting greeting(String name);
+}
