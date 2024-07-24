e3ef3f445252257779231924ca5572e05ecc2859
Lucas Lima <lukkasoliveiralima@gmail.com>
2024-07-24 17:18:40 -0300

    add get person endpoint doc

--- src/main/java/com/lukkas_lukkas/java_rest_api/infrastructure/http/controllers/person/GetPersonController.java
+++ src/main/java/com/lukkas_lukkas/java_rest_api/infrastructure/http/controllers/person/GetPersonController.java
@@ -3,6 +3,7 @@ package com.lukkas_lukkas.java_rest_api.infrastructure.http.controllers.person;
 import com.lukkas_lukkas.java_rest_api.application.person.get_person.GetPersonHandler;
 import com.lukkas_lukkas.java_rest_api.domain.Person;
 import com.lukkas_lukkas.java_rest_api.domain.exceptions.DataNotFoundException;
+import com.lukkas_lukkas.java_rest_api.infrastructure.http.contracts.person.GetPersonInterface;
 import com.lukkas_lukkas.java_rest_api.infrastructure.http.presenters.PersonView;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/person/{id}")
-public class GetPersonController {
+public class GetPersonController implements GetPersonInterface {
 
     private final GetPersonHandler handler;
 
@@ -19,7 +20,7 @@ public class GetPersonController {
     }
 
     @GetMapping
-    public ResponseEntity<?> get(@PathVariable(value = "id") String id) {
+    public ResponseEntity<PersonView> get(@PathVariable(value = "id") String id) {
         try {
             Person person = this.handler.get(id);
 
